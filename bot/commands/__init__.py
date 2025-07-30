b704557c8d09fa75c990402c6afbf2449b8361e1
PapaBearDoes <papabeardoes@gmail.com>
2025-07-30 06:55:36 -0700

    Update __init__.py

--- bot/commands/__init__.py
+++ bot/commands/__init__.py
@@ -1,7 +1,9 @@
 from .crisis_commands import CrisisKeywordCommands
 from .monitoring_commands import MonitoringCommands
+from .ensemble_commands import EnsembleCommands  # NEW: Three-model ensemble commands
 
 __all__ = [
     'CrisisKeywordCommands',
     'MonitoringCommands', 
+    'EnsembleCommands'  # Replaced EnhancedLearningCommands with EnsembleCommands
 ]
