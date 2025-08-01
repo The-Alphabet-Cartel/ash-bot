2f9d8cbe8efae3afbf66abc6a58eb26fd719bb2e
PapaBearDoes <papabeardoes@gmail.com>
2025-07-31 18:14:39 -0700

    Update __init__.py

--- bot/services/__init__.py
+++ bot/services/__init__.py
@@ -2,10 +2,8 @@
 Service layer components for Ash Bot v3.0
 """
 
-from .detection_service import DetectionService
 from .rate_limit_service import RateLimitService
 
 __all__ = [
-    'DetectionService',
     'RateLimitService'
 ]
